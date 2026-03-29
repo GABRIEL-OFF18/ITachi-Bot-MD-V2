fe360f531f1b01cf9817b7f2010859de5a926f5f
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-03-29 16:29:27 -0500

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `${emojis} Pikachu-bot se despide.`) 
+await conn.reply(id, `${emojis} Itachi-bot se despide.`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
